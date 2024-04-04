1f3209d3312f4d6fed5945f83f1e091d46a9a63d
Swati Saxena <swati.lpu17@gmail.com>
2024-04-04 13:40:30 -0700

    Updated dependencies & added antd for css styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import "./App.css";
+import { Button, DatePicker } from 'antd';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1> Employee Management System</h1>
+      <Button type='primary'>Primary Button</Button>
+      <DatePicker />
     </div>
   );
 }
