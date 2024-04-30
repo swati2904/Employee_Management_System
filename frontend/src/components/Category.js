5d6b397af026ae25887eb07114ed81276a16ced7
Swati Saxena <swati.lpu17@gmail.com>
2024-04-29 18:13:35 -0700

    updated category component

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,32 +1,41 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Table } from 'antd';
-import './style.css';
+import { Card } from 'antd';
+
+const { Meta } = Card;
 
 const Category = () => {
-  const [category, setCategory] = useState([]);
+  const [categoriesWithEmployees, setCategoriesWithEmployees] = useState([]);
 
   useEffect(() => {
+    // Fetch employees and categories data from backend
     axios
-      .get('http://localhost:3001/auth/category')
-      .then((result) => {
-        if (result.data.Status) {
-          setCategory(result.data.Result);
-        } else {
-          alert(result.data.Error);
-        }
+      .get('http://localhost:3001/auth/employee')
+      .then((employeeResult) => {
+        axios
+          .get('http://localhost:3001/auth/category')
+          .then((categoryResult) => {
+            const categories = categoryResult.data.Result;
+            // Group employees by category ID
+            const employeesGroupedByCategory = {};
+            employeeResult.data.Result.forEach((employee) => {
+              if (!employeesGroupedByCategory[employee.category_id]) {
+                employeesGroupedByCategory[employee.category_id] = [];
+              }
+              employeesGroupedByCategory[employee.category_id].push(employee);
+            });
+            // Match category name with employees
+            const categoriesWithEmployeesData = categories.map((category) => ({
+              categoryName: category.name,
+              employees: employeesGroupedByCategory[category.id] || [],
+            }));
+            // Set state with categories and corresponding employees
+            setCategoriesWithEmployees(categoriesWithEmployeesData);
+          })
+          .catch((err) => console.log(err));
       })
       .catch((err) => console.log(err));
   }, []);
-
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-  ];
 
   return (
     <div className='categoryContainer'>
@@ -34,11 +43,33 @@ const Category = () => {
         <div className='headingContainer'>
           <h3>Category List</h3>
         </div>
-        <Button className='primaryBtn'>
-          <Link to='/dashboard/add_category'>Add Category</Link>
-        </Button>
-        <div className='categoryTable'>
-          <Table dataSource={category} columns={columns} pagination={true} />
+        <div className='categoryCards'>
+          {categoriesWithEmployees.map((category) => (
+            <Card
+              key={category.categoryName}
+              title={category.categoryName}
+              className='categoryCard'
+            >
+              <div className='employeeList'>
+                {category.employees.map((employee) => (
+                  <div key={employee.id} className='employeeItem'>
+                    {/* <img
+                      alt='employee'
+                      src={`http://localhost:3001/Images/${employee.image}`}
+                      className='employeeImage'
+                    /> */}
+                    <div className='employeeDetails'>
+                      <p className='employeeName'>{employee.name}</p>
+
+                      {/* <p className='employeeCategory'>
+                        Category: {category.categoryName}
+                      </p> */}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </Card>
+          ))}
         </div>
       </div>
     </div>
